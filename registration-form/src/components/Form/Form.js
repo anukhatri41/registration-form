e5dfe6fa66f5c963011041be7d583775d9bc536e
anukhatri41 <anukhatri41@gmail.com>
2023-12-30 17:06:30 -0600

    State Setting - Form.js

--- registration-form/src/components/Form/Form.js
+++ registration-form/src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Form.css';
 import Label from '../Label/Label';
 import InputText from '../InputText/InputText';
@@ -8,25 +8,39 @@ import Button from '../Button/Button';
 
 
 function Form() {
+  const [state, setState] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    username: "",
+  });
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    });
+  };
+  
   return (
     <form>
-      <Label htmlFor="first-name" value="First Name" />
-      <InputText placeholder="First Name" id="first-name"/>
+      <Label htmlFor="first_name" value="First Name" />
+      <InputText placeholder="First Name" name="first_name" id="first_name" value={state.first_name} setValue={handleChange }/>
 
-      <Label htmlFor="last-name" value="Last Name" />
-      <InputText placeholder="Last Name" id="last-name"/>
+      <Label htmlFor="last_name" value="Last Name" />
+      <InputText placeholder="Last Name" name="last_name" id="last_name" value={state.last_name}/>
 
       <Label htmlFor="email" value="Email" />
-      <InputEmail placeholder="Email" id="email"/>
+      <InputEmail placeholder="Email" name="email" id="email" value={state.email}/>
 
       <Label htmlFor="username" value="Username" />
-      <InputText placeholder="Username" id="username"/>
+      <InputText placeholder="Username" name="username" id="username" value={state.username}/>
 
       <Label htmlFor="password" value="Password" />
       <InputPassword placeholder="Password" id="password"/>
 
-      <Label htmlFor="retype-password" value="Retype Password" />
-      <InputPassword placeholder="Retype Password" id="retype-password"/>
+      <Label htmlFor="retype_password" value="Retype Password" />
+      <InputPassword placeholder="Retype Password" id="retype_password"/>
 
     </form>
   );
